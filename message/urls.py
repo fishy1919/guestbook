01d2a106bd44b23f9a70629f0f59b4240858797b
Fishy Chou <huiyu1919@gmail.com>
2024-07-05 11:13:42 +0800

    d

--- message/urls.py
+++ message/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', MessageList.as_view(), name='msg_list'), 
     path('<int:pk>/', MessageView.as_view(), name='msg_view'),
     path('create/', MessageCreate.as_view(), name='msg_create'),
-    path('<int:pk>/delete/', views.MessageDelete.as_view(), name='msg_delete')
+    path('<int:pk>/delete/',MessageDelete.as_view(), name='msg_delete')
 ]
